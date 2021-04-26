c14d917a2417546aa5ca60f0abdf2ba7ff4acdb3
Huynh Duc Dung <dung@aircarbon.co>
2021-04-27 02:58:47 +0800

    feat: add youtube channel on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -48,6 +48,25 @@ export default function Header({ siteTitle }: { siteTitle: string }) {
             </a>
             <a
               className="flex items-center mx-2 text-gray-800 hover:text-indigo-600"
+              href="https://www.youtube.com/channel/UC277tIVuzLYuFDU23kTXltg"
+            >
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
+                />
+              </svg>
+            </a>
+            <a
+              className="flex items-center mx-2 text-gray-800 hover:text-indigo-600"
               href="https://github.com/jellydn"
             >
               <svg
