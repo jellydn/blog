790b0e91b5df5c5eef58964590a6098a192605a4
Huynh Duc Dung <dunghd.it@gmail.com>
2021-10-27 23:48:48 +0800

    :heavy_plus_sign: Update React 18

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
+// next.config.js
 module.exports = {
+    experimental: {
+        concurrentFeatures: false,
+        serverComponents: false,
+    },
+    images: {
+        domains: ['gyazo.com'],
+    },
     webpack(config) {
         config.module.rules.push({
             test: /\.md$/,
             use: 'raw-loader',
         });
         return config;
-    },
-    images: {
-        domains: ['gyazo.com'],
     },
 };
