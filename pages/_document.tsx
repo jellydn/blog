b648079e7a59b7c615a8af6415450ffffe61a288
Huynh Duc Dung <dunghd.it@gmail.com>
2023-01-16 18:32:27 +0800

    fix(ci): build on vercel

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,7 +25,12 @@ class MyDocument extends Document {
                         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5337133458846513"
                         crossOrigin="anonymous"
                     />
-                    <script async defer src="https://analytics.umami.is/script.js" dataWebsiteId="94f78c78-3384-4c3b-9a26-a9cde3b9075f" />
+                    <script
+                        async
+                        defer
+                        src="https://analytics.umami.is/script.js"
+                        data-website-id="94f78c78-3384-4c3b-9a26-a9cde3b9075f"
+                    />
                 </body>
             </Html>
         );
